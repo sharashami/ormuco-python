a9e8ac8fc21da2de59f26aa4bcac50c3b3abf119
Shara Shami <shara.alves@gmail.com>
2019-04-03 20:34:16 -0300

    compare method implemented and some new test cases were added related to different type inputs

--- b-software-library/src/stringutils.py
+++ b-software-library/src/stringutils.py
@@ -1,9 +1,18 @@
 def compare(str1, str2):
-    
+    to_string = lambda inp : str(inp) if type(inp) != str else inp
+
     if str1 == None or str2 == None:
         raise ValueError()
-    
-    
-    
 
-#future features can be added here
+    str1=to_string(str1)
+    str2=to_string(str2)
+    
+    for char1, char2 in zip(str1, str2):
+        ascii1 = ord(char1)
+        ascii2 = ord(char2)
+        if ascii1 != ascii2:
+            return 1 if ascii1 > ascii2 else -1
+
+    return 0 if len(str1) == len(str2) else (1 if len(str1) > len(str2) else -1)
+
+     
