3b9bc5ae5864f552d94235e6fba466d2cfa2349c
sharashami <shara.alves@gmail.com>
2019-04-04 20:52:23 -0300

    question B done, feature built as a module you can use right away

--- b-software-library/src/stringutils.py
+++ b-software-library/src/stringutils.py
@@ -1,8 +1,23 @@
 def compare(str1, str2):
+    """Returns whether one is greater than, equal, or less than the other.
+    
+    Args:
+        str1 (str): The string which is to be compared with.
+        str2 (str): The other string.
+
+    Raises:
+        ValueError: If str1 or str2 is equal to None.
+
+    Returns:
+        -1 if str1 is less than str2.
+        0 if str1 is equal to str2.
+        1 if str1 is greater than str2.
+
+    """
     to_string = lambda inp : str(inp) if type(inp) != str else inp
 
     if str1 == None or str2 == None:
-        raise ValueError()
+        raise ValueError("You should give me 2 version string as input and returns whether one is greater than, equal, or less than the other. As an example: '1.2' is greater than '1.1'")
 
     str1=to_string(str1)
     str2=to_string(str2)
@@ -15,4 +30,4 @@ def compare(str1, str2):
 
     return 0 if len(str1) == len(str2) else (1 if len(str1) > len(str2) else -1)
 
-     
+
